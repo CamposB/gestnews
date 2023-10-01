fa088dddfa714eb663b33d953cff91c98dbaf42b
Marco Antonio Gimenes Meira <mgmarco38@gmail.com>
2023-10-01 14:12:26 -0300

    Estilizando o footer
    Estilizando o footer para apresentarção, adicionando css interno no rodape.php

--- templates/rodape.php
+++ templates/rodape.php
@@ -1,12 +1,50 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Minha Página</title>
 
-		<footer>
-			<h4>GestNews</h4>
-			<br>
-			<h5>Powered By - Projacks Tech &copy; 2023</h5>
-			<H6>Eleve o Futuro!</H6>
-		
-		</footer>
-	
-	</body>
+    <!-- CSS interno -->
+    <style>
+        /* Estilos para o elemento footer */
+        .footer {
+            text-align: center;
+            background-color: #333;
+            color: white;
+            padding: 20px;
+        }
 
+		/* Estilos para os títulos no rodapé */
+		.footer h4,
+		.footer h5,
+		.footer h6 {
+			margin: 10px 0; /* Espaçamento entre os elementos do rodapé */
+		}
+
+		/* Estilos específicos para o título principal (h4) */
+		.footer h4 {
+			font-size: 24px; /* Tamanho da fonte */
+		}
+
+		/* Estilos específicos para o texto de direitos autorais (h5) */
+		.footer h5 {
+			font-size: 18px; /* Tamanho da fonte */
+		}
+
+		/* Estilos específicos para o texto de slogan (h6) */
+		.footer h6 {
+			font-size: 14px; /* Tamanho da fonte */
+			font-style: italic; /* Estilo itálico */
+		}
+    </style>
+</head>
+<body>
+	<footer class="footer">
+		<h4>GestNews</h4>
+		<img src="images/logo-projacks-png.png" alt="Descrição da Imagem" width="150" height="150">
+		<h5>Powered By - Projacks Tech &copy; 2023</h5>
+		<h6>Eleve o Futuro!</h6>
+	</footer>
+</body>
 </html>
