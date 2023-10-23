486f00577f98d58437b4e1ab63c1944c68be5efe
Marco Antonio Gimenes Meira <mgmarco38@gmail.com>
2023-10-23 18:51:28 -0300

    Finalizando o formulario
    Finalizando o formulario todo

--- insertPost.php
+++ insertPost.php
@@ -6,14 +6,17 @@
     <style>
         * {
             font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-            margin: 0px;
-            padding: 0px;
+            margin: 0;
+            padding: 0;
         }
 
-        body{
+        body {
             width: 100%;
-            height: 100vh;
+            min-height: 100vh;
             background-color: #f9bb74;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
         }
 
         h2 {
@@ -22,33 +25,18 @@
             color: white;
         }
 
-        input[type="submit"] {
-            border: none;
-            background-color: #6c63ff;
-            padding: 0.62rem;
-            cursor: pointer;
-            border-radius: 5px;
-            color: #fff;
-            width: 100%;
-            margin-top: 2.5rem;
-            border: none;
-            
-        }
-
-        input[type="submit"]:hover {
-            background-color: #f9bb74;
-        }
-
-        .container-dois{
+        .container-dois {
             width: 80%;
-            height: 80vh;
+            min-height: 80vh;
             display: flex;
-            box-shadow: 5px 5px 10px rgba(0, 0, 0, .212);
+            box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.212);
             margin: 50px;
-            
+            background: white;
+            border-radius: 15px;
+            overflow: hidden;
         }
 
-        .form-image{
+        .form-image {
             width: 50%;
             display: flex;
             justify-content: center;
@@ -57,50 +45,68 @@
             padding: 1rem;
         }
 
-        .form-image img{
-            width: 31rem;
+        .form-image img {
+            width: 80%;
+            max-width: 300px;
         }
 
-        .form{
+        .form {
             width: 50%;
             display: flex;
-            justify-content: center;
-            align-items: center;
             flex-direction: column;
-            background-color: #fff;
-            padding: 3rem;
+            padding: 2rem;
         }
 
-        .input-group{
+        .input-group {
             display: flex;
             flex-wrap: wrap;
-            justify-content: space-between;
-            padding: 1rem 0;
         }
 
-        .input-box{
+        .input-box {
+            flex: 1;
             display: flex;
             flex-direction: column;
-            margin-bottom: 1.1rem;
+            margin: 1.1rem;
         }
 
+        .input-box label {
+            font-weight: bold;
+        }
 
-        .input-box input{
+        .input-box input, .input-box textarea, .input-box select {
             margin: 0.6rem 0;
             padding: 0.8rem 1.2rem;
             border: none;
             border-radius: 10px;
             box-shadow: 1px 1px 6px #0000001c;
-            size: 500px;
         }
-        
+
+        input[type="file"] {
+            padding: 0;
+        }
+
+        input[type="submit"] {
+            border: none;
+            background-color: #6c63ff;
+            padding: 0.62rem;
+            cursor: pointer;
+            border-radius: 10px;
+            color: #fff;
+            width: 100%;
+            margin-top: 2.5rem;
+        }
+
+        input[type="submit"]:hover {
+            background-color: #f9bb74;
+        }
+
         @media screen and (max-width: 1330px) {
-            .form-image{
+            .form-image {
                 display: none;
-            }    
-            
-            .container-dois{
-                width: 50%;
+            }
+
+            .container-dois {
+                width: 80%;
             }
 
             .form {
@@ -108,45 +114,40 @@
             }
         }
 
-        @media screen and (max-width: 1064px){
-
-            .container-dois{
+        @media screen and (max-width: 1064px) {
+            .container-dois {
                 width: 90%;
-                height: auto;
+                min-height: auto;
             }
 
-            .input-group{
-                flex-direction:column;
-                overflow-y: scroll;
-                flex-wrap: nowrap;
-                max-width: 10rem;
+            .input-group {
+                flex-direction: column;
             }
         }
-
     </style>
 </head>
 <body>
     <h2>Inserir Notícia</h2>
-        <div class="container-dois">
-            <div class="form-image">
-                <img src="images/undraw_businessman_e7v0.svg">
-            </div>
-            <div class="form">
-                <form method="POST" action="process.php" enctype="multipart/form-data">
+    <div class="container-dois">
+        <div class="form-image">
+            <img src="images/undraw_businessman_e7v0.svg">
+        </div>
+        <div class="form">
+            <form method="POST" action="process.php" enctype="multipart/form-data">
                 <div class="input-group">
                     <div class="input-box">
                         <label for="title">Título:</label>
-                        <input type="text" name="title" id="title" required><br><br>
+                        <input type="text" name="title" id="title" required>
                     </div>
 
                     <div class="input-box">
-                        <label for="notice">Notícia:</label><br>
-                        <textarea name="notice" id="notice" rows="4" cols="100" required></textarea><br><br>
+                        <label for="notice">Notícia:</label>
+                        <textarea name="notice" id="notice" rows="4" cols="50" style="resize: none" required></textarea>
                     </div>
 
                     <div class="input-box">
                         <label for="author">Autor:</label>
-                        <input type="text" name="author" id="author" required><br><br>
+                        <input type="text" name="author" id="author" required>
                     </div>
 
                     <div class="input-box">
@@ -156,31 +157,23 @@
                             <option value="Policial">Policial</option>
                             <option value="Entretenimento">Entretenimento</option>
                             <option value="Outras">Outras</option>
-                        </select><br><br>
+                        </select>
                     </div>
 
                     <div class="input-box">
                         <label for="image">Imagem:</label>
-                        <input type="file" name="image" id="image" accept="image/*" required><br><br>
+                        <input type="file" name="image" id="image" accept="image/*" required>
                     </div>
 
                     <div class="input-box">
                         <label for="video">Código HTML do Vídeo:</label>
-                        <textarea id="video" name="video" rows="2" cols="50"></textarea>
+                        <textarea id="video" name="video" rows="2"></textarea>
                     </div>
 
-                    <div class="continue-button">
-                        <input type="submit" value="Inserir Notícia">
-                    </div>
+                    <input type="submit" value="Inserir Notícia">
                 </div>
-                </form>
-            </div>
+            </form>
         </div>
+    </div>
 </body>
 </html>
-
-
-
-<?php 
-include_once("templates/rodape.php");
-?>
